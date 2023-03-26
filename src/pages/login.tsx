fecd49ff48c4b13fa05f98148880cadf463bac0a
Frank <40611willy@gmail.com>
2023-03-26 17:31:05 +0800

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,7 +9,7 @@ export default function Login() {
 
     const signInWithGithub = (e: any) => {
         e.preventDefault();
-        signIn("github");
+        signIn();
     }
 
     if (serverService.isLogin)

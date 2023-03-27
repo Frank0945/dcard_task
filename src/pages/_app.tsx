99b5c96301d22b9149bc6a690e83b75f080ebcde
Frank <40611willy@gmail.com>
2023-03-28 03:24:30 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { getSession, SessionProvider } from "next-auth/react"
+import { getSession, SessionProvider, useSession } from "next-auth/react"
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import DialogController from "@/components/dialogs/dialogs"
@@ -16,30 +16,32 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 
   const isLoginPage = router.pathname == "/login";
 
-
-  const [sessionData, setSessionData] = useState(null);
-
   useEffect(() => {
-    async function getSessionData() {
-      // 获取会话信息
-      const res = await fetch('/api/auth/session');
-      const sessionData = await res.json();
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
 
-      serverService.session = sessionData;
-
-      // 存储会话信息
-      setSessionData(sessionData);
-      localStorage.setItem('sessionData', JSON.stringify(sessionData));
-    }
-
-    getSessionData();
+    /*console.log(session);
+    serverService.session = session;*/
   }, []);
 
+
+  function Status({ children }: any) {
+    const session = useSession();
+    if (session.status === "loading") {
+      return <>Loading</>
+    }
+    serverService.session = session.data;
+    return (
+      <>
+        {!isLoginPage && <Navbar />}
+        <Component {...pageProps} />
+        <DialogController />
+      </>
+    )
+  }
+
   return (
-    <SessionProvider session={sessionData} >
-      {!isLoginPage && <Navbar />}
-      <Component {...pageProps} />
-      <DialogController />
+    <SessionProvider session={session} >
+      <Status />
     </SessionProvider >
   )
   /* useEffect(() => {

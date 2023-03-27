e3a666d91001a76a7540239ac38bdecb07b70b1c
Frank <40611willy@gmail.com>
2023-03-28 01:01:42 +0800

    vercel test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,13 @@ import styles from '@/styles/pages/Home.module.css'
 import Head from 'next/dist/shared/lib/head'
 import PostTask from '@/components/task/postTask'
 import TaskList from '@/components/task/taskList'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { serverService } from '@/services/serverService';
 
 export default function Home() {
 
   const [reload, setReload] = useState(0);
-  const [isInit, setIsInit] = useState(false);
-
-  useEffect(() => {
-    serverService.init().then(() => {
-      setIsInit(true);
-    });
-  }, [])
+  const session = useSession();
 
   const handlePosted = () => {
     setReload(reload + 1);
@@ -27,8 +20,9 @@ export default function Home() {
         <title>Task</title>
       </Head>
       <main className={styles.main}>
-        {isInit ?
+        {session.data ?
           <>
+          {session.data.accessToken}
             <PostTask onPosted={handlePosted} />
             <TaskList reload={reload} />
           </> : <div>Not logged in</div>

a3803f5d7ba38113fc262770ce2c9a249ffcb32a
Frank <40611willy@gmail.com>
2023-03-27 19:19:52 +0800

    upgrade next-auth & add console.log in nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,6 +24,7 @@ export default NextAuth({
             return token;
         },
         async session({ session, token }) {
+            console.log("NEXTAUTH_URL", process.env.NEXTAUTH_URL);
             session.accessToken = token.accessToken as string;
             return session;
         }
